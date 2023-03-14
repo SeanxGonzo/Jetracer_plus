6161103ef781b78be94f1c62f8ad46bca8b02e60
SeanxGonzo <125489201+SeanxGonzo@users.noreply.github.com>
2023-03-14 08:16:51 -0600

    Sean
    Changed READ.me file, and added command script.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Jetracer_plus
 Code to augment the capabilities of the Jetracer, Authored by myself.
+#Sean Gonzales
+make robot go in circle
---
+++ pathfinders_vehicle_control_SG.py
@@ -0,0 +1,11 @@
+
+#Pathfinders_vehicle_control_SG.py
+
+#importing required libraries
+from jetracer.nvidia_racecar Import NvidiaRacecar
+
+# Instantiate the object for Object Oriented Programming (OOP)
+car = NvidiaRacecar()
+
+# Our first command of the vehicle
+car.steering = 0.3
